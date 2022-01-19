0c356e82ecf396bf3b6af87e24946050b86b0aba
HadHod <maciej.mlynarski@gmail.com>
2022-01-19 11:06:46 +0100

    project init

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+const MAT_MODULES = [
+  MatSliderModule,
+  MatInputModule,
+  MatIconModule,
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MAT_MODULES,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
