da6caafda883392560d852ed54173aa7322efb05
HadHod <maciej.mlynarski@gmail.com>
2022-01-28 17:44:49 +0100

    dividend discount model - fair value matrix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public ticker: string = '';
+  public dividend: number = 2.536;
+  public growth: number = 2;
+  public return: number = 7;
+  public range: number = 4;
+
+  get veryTop(): number {
+    return 1 + (1 * this.range / 100 * 2);
+  }
+
+  get top(): number {
+    return 1 + (1 * this.range / 100);
+  }
+
+  get bottom(): number {
+    return 1 - (1 * this.range / 100);
+  }
+
+  get veryBottom(): number {
+    return 1 - (1 * this.range / 100 * 2);
+  }
+
+  public getFairValue(growthRate: number, returnRate: number): number {
+    const growthAdjusted: number = this.growth * growthRate;
+    const returnAdjusted: number = this.return * returnRate;
+
+    const numerator: number = this.dividend * (1 + growthAdjusted / 100);
+    const denominator: number = returnAdjusted / 100 - growthAdjusted / 100;
+
+    return Math.round((numerator / denominator) * 100) / 100;
+  }
 }
