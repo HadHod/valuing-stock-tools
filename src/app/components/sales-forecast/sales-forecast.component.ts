b24a5f652533fdd26bec70cab0b2bcfa98e6d450
HadHod <maciej.mlynarski@gmail.com>
2022-04-09 15:35:12 +0200

    sales forecast added

--- src/app/components/sales-forecast/sales-forecast.component.ts
+++ src/app/components/sales-forecast/sales-forecast.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-sales-forecast',
   templateUrl: './sales-forecast.component.html',
   styleUrls: ['./sales-forecast.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SalesForecastComponent implements OnInit {
+export class SalesForecastComponent {
+  public marketCap: number = 12.345;
+  public year: number = new Date().getFullYear();
+  public margin: number[] = [10, 15, 20];
+  public revenue: number[] = [1, 2, 3, 4, 5];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public calculatePriceToEarnings(revenueIndex: number, marginIndex: number): number {
+    return Math.round(this.marketCap / (this.revenue[revenueIndex] * this.margin[marginIndex] / 100));
   }
-
 }
